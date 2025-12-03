38a3011941e3e09ace58ef491af1ac05b5679808
JangWh <janghy95@naver.com>
2025-12-03 14:45:19 +0900

    에너지 회복 시간 표시 작업중
    1. 클래스 생성

---
+++ Assets/_Proj/Scripts/UI/Comon/EnergyRecoveryTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class EnergyRecoveryTime : MonoBehaviour
+{
+    public TextMeshProUGUI energyRecoveryTime;
+
+    void Awake()
+    {
+        EnergyRecoverTime();
+    }
+
+    void EnergyRecoverTime()
+    {
+        if (UserData.Local.goods[GoodsType.energy] > 5) return;
+
+        long time = UserData.Local.master.lastEnergyTime;
+
+        DateTimeOffset dto = new DateTimeOffset(time, TimeSpan.Zero);
+        print($"{dto} --> Unix Seconds: {dto.ToUnixTimeSeconds()}");
+
+
+
+    }
+}
